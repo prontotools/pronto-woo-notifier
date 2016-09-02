a49c9cdc724731ab58ecaf6860f2223771842299
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-02 13:15:26 +0700

    Fix site to not inherit from inherit_resource :muscle:

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,6 @@
 class Site < ApplicationRecord
-    has_many :plugin_trackers
-    has_many :plugins, through: :plugin_trackers
+    # has_many :plugin_trackers
+    # has_many :plugins, through: :plugin_trackers
 
     validates :name, presence: true
     VALID_DOMAIN_REGEX = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
