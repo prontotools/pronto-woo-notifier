20ad58e126e36131f31446535a78c2fa40ec261f
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-02 13:51:36 +0700

    Fix plugintracker :muscle:

--- app/models/plugin_tracker.rb
+++ app/models/plugin_tracker.rb
@@ -2,7 +2,5 @@ class PluginTracker < ApplicationRecord
     belongs_to :site
     belongs_to :plugin
 
-    validates :site, presence: true
-    validates :plugin, presence: true
     validates :current_version, presence: true
 end
