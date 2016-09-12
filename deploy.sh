d1e558e065de7067884a4388929c50f1d8101555
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-12 13:39:21 +0700

    Try using deploy keyword in travis'

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-TARGET_BRANCH="master"
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o $TRAVIS_BRANCH != $TARGET_BRANCH] ; then
-  echo "Skipping deploy, Just doing a build"
-  exit 0
-fi
-
 docker build -t $REPO:$COMMIT -f ./compose/rails/Dockerfile .
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker push $REPO
