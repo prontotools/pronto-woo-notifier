5ea2987f8849c8101ee6204f9bf7f3ad1e36b035
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-12 13:12:50 +0700

    Don deploy on pull request

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ "$TRAVIS_PULL_REQUEST" != "false"] ; then
+  echo "Skipping deploy on pull request"
+  exit 0
+fi
+
+
 if [ $TRAVIS_BRANCH == 'master' ] ; then
   docker build -t $REPO:$COMMIT -f ./compose/rails/Dockerfile .
   docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
