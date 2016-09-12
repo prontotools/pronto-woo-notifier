3a9b54986f2d797574567b01324e65af4e33b91d
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-12 12:51:58 +0700

    Use custom deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-docker build -t $REPO:$COMMIT -f ./compose/rails/Dockerfile .
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-docker push $REPO
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  docker build -t $REPO:$COMMIT -f ./compose/rails/Dockerfile .
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+  docker push $REPO
+fi
