08ca954fd1500301570d368129adab46ed440686
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-12 13:18:22 +0700

    Make it not deplot on pull request and brach is not master

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" != "false"] ; then
-  echo "Skipping deploy on pull request"
+TARGET_BRANCH="master"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o $TRAVIS_BRANCH != $TARGET_BRANCH] ; then
+  echo "Skipping deploy, Just doing a build"
   exit 0
 fi
 
-
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-  docker build -t $REPO:$COMMIT -f ./compose/rails/Dockerfile .
-  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-  docker push $REPO
-fi
+docker build -t $REPO:$COMMIT -f ./compose/rails/Dockerfile .
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+docker push $REPO
