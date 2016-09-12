13792839fee274a2423684513a75fdd379fe9bac
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-12 14:34:30 +0700

    Deploy on master branch only

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o  "$TRAVIS_BRANCH" != "master" ]; then
+    echo "Skiping Deploy, Just run test"
+    exit 0
+fi
+
 docker build -t $REPO:$COMMIT -f ./compose/rails/Dockerfile .
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker push $REPO
