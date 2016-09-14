b4845dba33381a22043037bc7afc793f8c636746
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-14 15:42:07 +0700

    Establish connection only production

--- config/puma.rb
+++ config/puma.rb
@@ -22,8 +22,10 @@ pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection
+if rails_env == "production"
+  on_worker_boot do
+    require "active_record"
+    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  end
 end
