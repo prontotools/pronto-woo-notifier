c41dcdbcf4be8a87f3047b316f71a2697356b76d
Tanapong Udomrattanasirichai <Prince@PRONTOLAP0026.local>
2016-09-15 11:10:19 +0700

    Refactor ci to build only one time when merge to master

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o  "$TRAVIS_BRANCH" != "master" ]; then
 fi
 
 echo "Latest version: $COMMIT "
-docker build -t $REPO:$COMMIT -f ./compose/rails/Dockerfile .
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker push $REPO
 
