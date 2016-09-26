69b3ed4934873c2e0447036117dbac4bd5f18e79
nattyait <nattanicha@gmail.com>
2016-09-26 17:02:44 +0700

    Expose API for sites :baby:

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/plugins/sync', to: 'plugins#sync'
   get '/sites/sync', to: 'sites#sync_all', as: "sites_sync"
   get '/sites/:id/sync', to: "sites#sync_each", as: "site_sync"
+  get '/sites/api', to: "sites#api", as: "site_api"
 
   resources :plugins, only: [:index]
   resources :sites
